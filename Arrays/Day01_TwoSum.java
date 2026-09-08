94e96f3705aa89a4edd23aacbdff55a78cdb6f23
Suman Paramanik <paramaniksuman623@gmail.com>
2026-09-08 21:49:30 +0530

    Day 1: Added Two Sum

---
+++ Arrays/Day01_TwoSum.java
@@ -0,0 +1,28 @@
+public class Day01_TwoSum {
+
+    public static int linearSearch(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 30, 45, 50};
+        int target = 30;
+
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
