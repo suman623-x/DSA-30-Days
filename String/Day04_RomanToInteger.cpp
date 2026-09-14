3302f0462efc49f5db3e6d14e8363481c7374bfa
Suman Paramanik <paramaniksuman623@gmail.com>
2026-09-14 22:33:00 +0530

    Add Roman to Integer solution

---
+++ String/Day04_RomanToInteger.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char, int> value = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i + 1 < s.length() &&
+                value[s[i]] < value[s[i + 1]]) {
+
+                ans -= value[s[i]];
+
+            } else {
+                ans += value[s[i]];
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s = "MCMXCIV";
+
+    cout << "Roman: " << s << endl;
+    cout << "Integer: " << obj.romanToInt(s) << endl;
+
+    return 0;
+}
