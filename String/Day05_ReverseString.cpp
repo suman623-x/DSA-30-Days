6878b0ea2690a0c8b0b943ab218e6a0ae632d5c4
Suman Paramanik <paramaniksuman623@gmail.com>
2026-09-15 22:56:22 +0530

    Day 05: Reverse String

---
+++ String/Day05_ReverseString.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+
+    Solution obj;
+    obj.reverseString(s);
+
+    cout << "Reversed string: ";
+
+    for (char ch : s) {
+        cout << ch;
+    }
+
+    cout << endl;
+
+    return 0;
+}
