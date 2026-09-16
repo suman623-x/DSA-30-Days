b9a644ce66c0bbd436fbf22d8be814d9590a1ae8
Suman Paramanik <paramaniksuman623@gmail.com>
2026-09-16 21:57:14 +0530

    Add Day 04 To Lower Case

---
+++ String/Day04_To_Lower_Case.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class solution {
+        public:
+        string toLowerCase(string s){
+            for(int i =0;i<s.length();i++){
+                if(s[i]>='A' && s[i]<='Z'){
+                    s[i] = s[i] + 32 ;
+                }
+            }
+            return s ;
+        }
+};
